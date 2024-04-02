8a7bac333fc4e13929a0d08528f42f8f42cc30fc
AkashDataScience <akashmshah19@gmail.com>
2024-04-02 20:35:31 +0530

    Minor update

--- utils.py
+++ utils.py
@@ -80,7 +80,7 @@ def get_ReduceLROnPlateau_scheduler(optimizer, factor, patience):
     scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor=factor, patience=patience, verbose=True)
     return scheduler
 
-def get_OneCycleLR_scheduler(optimizer, max_lr, epochs, steps_per_epoch, max_at_epoch, div_factor, final_div_factor):
+def get_OneCycleLR_scheduler(optimizer, max_lr, epochs, steps_per_epoch, max_at_epoch, anneal_strategy, div_factor, final_div_factor):
     """Method to get object of scheduler class. Used to update learning rate
 
     Args:
@@ -89,6 +89,7 @@ def get_OneCycleLR_scheduler(optimizer, max_lr, epochs, steps_per_epoch, max_at_
         epochs (float): Total number of epoch
         steps_per_epoch (int): Total steps in an epoch
         max_at_epoch (int): Epoch to reach maximum learning rate
+        anneal_strategy (string): Strategy to interpolate between minimum and maximum lr
         div_factor (int): Divisive factor to calculate intial learning rate
         final_div_factor (int): Divisive factor to calculate minimum learning rate
 
@@ -97,7 +98,9 @@ def get_OneCycleLR_scheduler(optimizer, max_lr, epochs, steps_per_epoch, max_at_
     """
     scheduler = optim.lr_scheduler.OneCycleLR(optimizer, max_lr=max_lr,  epochs=epochs,
                                               steps_per_epoch=steps_per_epoch, 
-                                              pct_start=max_at_epoch/epochs, div_factor=div_factor,
+                                              pct_start=max_at_epoch/epochs,
+                                              anneal_strategy=anneal_strategy,
+                                              div_factor=div_factor,
                                               final_div_factor=final_div_factor)
     return scheduler
 
